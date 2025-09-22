c169f18e2ca921d614a45f6021c0ca80f0f55e49
Drisya-rekha <drisyar12@gmail.com>
2025-09-22 23:22:00 +0530

    Commit

---
+++ src/data/headerData.js
@@ -0,0 +1,10 @@
+import resume from "../assets/pdf/resume.pdf";
+
+export const headerData = {
+  name: "Nafla T N",
+  title: "Solution Engineer | Java Developer",
+  desciption:
+    "Engineer of code and curator of frames. When I’m not solving problems with Java, I’m framing stories through my lens.",
+  image: "/Profile.jpg",
+  resumePdf: resume,
+};
