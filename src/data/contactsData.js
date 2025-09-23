b1f868ac4f398ccd0763fc2f18fdb02fe4bd6113
Aarif Hussain <aarifhuss70349@gmail.com>
2025-09-23 12:04:16 +0530

    Contact

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,6 @@
 export const contactsData = {
-    email: 'janedoe.test@gmail.com',
-    phone: '+915588776600',
-    address: 'Menlo Park, California, United States - 673822 ',
+    email: 'naflanazar15@gmail.com',
+    address: 'Kochi Kerala, India',
 
     sheetAPI: ''
 }
