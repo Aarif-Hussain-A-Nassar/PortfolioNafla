b5b60827544b7629d7c0b9d4d0cfac10dff1e81e
Aarif Hussain <aarifhuss70349@gmail.com>
2025-09-23 09:55:41 +0530

    Experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,16 @@
 export const experienceData = [
-    {
-        id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
-    },
-    {
-        id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
-    },
-    {
-        id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
-        startYear: '2020',
-        endYear: 'Present'
-    },
-]
+  {
+    id: 1,
+    company: "IBS Software",
+    jobtitle: "Senior Solution Engineer",
+    startYear: "2023",
+    endYear: "Present",
+  },
+  {
+    id: 2,
+    company: "Viral Fission",
+    jobtitle: " Campus Ambassador",
+    startYear: "2019",
+    endYear: "2023",
+  },
+];
