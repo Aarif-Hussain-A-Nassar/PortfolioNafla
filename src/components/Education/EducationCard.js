26861128223c32ad656f1271ff29b2ff89240506
Aarif Hussain <aarifhuss70349@gmail.com>
2025-09-23 08:05:44 +0530

    Made ICON Changes

--- src/components/Education/EducationCard.js
+++ src/components/Education/EducationCard.js
@@ -14,9 +14,9 @@ function EducationCard({ id, institution, course, startYear, endYear }) {
 
     const useStyles = makeStyles((t) => ({
         educationCard : {
-            backgroundColor:theme.primary30,
+            backgroundColor:theme.primary,
             "&:hover": {
-                backgroundColor:theme.primary50,
+                backgroundColor:theme.tertiary80,
             },
         },
     }));
@@ -27,12 +27,12 @@ function EducationCard({ id, institution, course, startYear, endYear }) {
         <Fade bottom>
             <div key={id} className={`education-card ${classes.educationCard}`} >
                 <div className="educard-img" style={{backgroundColor: theme.primary}}>
-                    <img src={theme.type === 'light' ? eduImgBlack : eduImgWhite} alt="" />
+                    <img src={theme.type === 'light' ? eduImgWhite : eduImgBlack} alt="" />
                 </div>
                 <div className="education-details">
-                    <h6 style={{color: theme.primary}}>{startYear}-{endYear}</h6>
-                    <h4 style={{color: theme.tertiary}}>{course}</h4>
-                    <h5 style={{color: theme.tertiary80}}>{institution}</h5>
+                    <h6 style={{color: theme.textPrimary}}>{startYear}-{endYear}</h6>
+                    <h4 style={{color: theme.textPrimary}}>{course}</h4>
+                    <h5 style={{color: theme.textPrimary}}>{institution}</h5>
                 </div>
             </div>
         </Fade>        
